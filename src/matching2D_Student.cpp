5bb25b6d7905748b74240c6552a6aeb6666fd77f
jinchaolu <30065376+jinchaolu@users.noreply.github.com>
2020-04-05 15:51:37 -0400

    feat: Update Task MP.2
    Add the following keypoint detectors in file matching2D.cpp and enable string-based selection based on detectorType (HARRIS, FAST, BRISK, ORB, AKAZE, SIFT)

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -29,6 +29,13 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
         matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
         // cout << "FLANN matching";
     }
+    else
+    {
+
+        //...
+        // Throw invalid argument error
+        throw invalid_argument("Invalid matcher type: " + matcherType);
+    }
 
     // perform matching task
     if (selectorType.compare("SEL_NN") == 0)
@@ -63,6 +70,13 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
         }
         cout << "# keypoints removed = " << knn_matches.size() - matches.size() << endl;
         // EOF STUDENT TASK
+    }
+    else
+    {
+
+        //...
+        // Throw invalid argument error
+        throw invalid_argument("Invalid selector type: " + selectorType);
     }
 }
 
@@ -102,7 +116,7 @@ void descKeypoints(vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &descr
 
         //...
         // Throw invalid argument error
-        throw invalid_argument("Invalid detector type: " + descriptorType);
+        throw invalid_argument("Invalid descriptor type: " + descriptorType);
     }
 
     // perform feature description
@@ -148,6 +162,127 @@ void detKeypointsShiTomasi(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool b
         cv::drawKeypoints(img, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
         string windowName = "Shi-Tomasi Corner Detector Results";
         cv::namedWindow(windowName, 6);
+        imshow(windowName, visImage);
+        cv::waitKey(0);
+    }
+}
+
+//// TASK MP.2 -> add the following keypoint detectors in file matching2D.cpp and enable string-based selection based on detectorType
+//// -> HARRIS, FAST, BRISK, ORB, AKAZE, SIFT
+void detKeypointsHarris(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis=false) {
+
+    // Detector parameters
+    int blockSize = 2;     // for every pixel, a blockSize × blockSize neighborhood is considered
+    int apertureSize = 3;  // aperture parameter for Sobel operator (must be odd)
+    int minResponse = 100; // minimum value for a corner in the 8bit scaled response matrix
+    double k = 0.04;       // Harris parameter (see equation for details)
+
+    // Detect Harris corners and normalize output
+    cv::Mat dst, dst_norm, dst_norm_scaled;
+    dst = cv::Mat::zeros(img.size(), CV_32FC1);
+    cv::cornerHarris(img, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT);
+    cv::normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+    cv::convertScaleAbs(dst_norm, dst_norm_scaled);
+
+    // visualize results
+    if (bVis) {
+        string windowName = "Harris Corner Detector Response Matrix";
+        cv::namedWindow(windowName, 4);
+        cv::imshow(windowName, dst_norm_scaled);
+        cv::waitKey(0);
+    }
+
+    // STUDENTS NEET TO ENTER THIS CODE (C3.2 Atom 4)
+
+    // Look for prominent corners and instantiate keypoints
+    vector<cv::KeyPoint> keypoints;
+    double maxOverlap = 0.0; // max. permissible overlap between two features in %, used during non-maxima suppression
+    for (size_t j = 0; j < dst_norm.rows; j++)
+    {
+        for (size_t i = 0; i < dst_norm.cols; i++)
+        {
+            int response = (int)dst_norm.at<float>(j, i);
+            if (response > minResponse)
+            { // only store points above a threshold
+
+                cv::KeyPoint newKeyPoint;
+                newKeyPoint.pt = cv::Point2f(i, j);
+                newKeyPoint.size = 2 * apertureSize;
+                newKeyPoint.response = response;
+
+                // perform non-maximum suppression (NMS) in local neighbourhood around new key point
+                bool bOverlap = false;
+                for (auto it = keypoints.begin(); it != keypoints.end(); ++it)
+                {
+                    double kptOverlap = cv::KeyPoint::overlap(newKeyPoint, *it);
+                    if (kptOverlap > maxOverlap)
+                    {
+                        bOverlap = true;
+                        if (newKeyPoint.response > (*it).response)
+                        {                      // if overlap is >t AND response is higher for new kpt
+                            *it = newKeyPoint; // replace old key point with new one
+                            break;             // quit loop over keypoints
+                        }
+                    }
+                }
+                if (!bOverlap)
+                {                                     // only add new key point if no overlap has been found in previous NMS
+                    keypoints.push_back(newKeyPoint); // store new keypoint in dynamic list
+                }
+            }
+        } // eof loop over cols
+    }     // eof loop over rows
+
+    // visualize keypoints
+    if (bVis) {
+        windowName = "Harris Corner Detection Results";
+        cv::namedWindow(windowName, 5);
+        cv::Mat visImage = dst_norm_scaled.clone();
+        cv::drawKeypoints(dst_norm_scaled, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        cv::imshow(windowName, visImage);
+        cv::waitKey(0);
+    }
+    // EOF STUDENT CODE
+}
+
+//// TASK MP.2 -> add the following keypoint detectors in file matching2D.cpp and enable string-based selection based on detectorType
+//// -> HARRIS, FAST, BRISK, ORB, AKAZE, SIFT
+void detKeypointsModern(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, std::string detectorType, bool bVis=false){
+    if (detectorType.compare("FAST") == 0)
+    {
+        auto fast = cv::FastFeatureDetector::create(); 
+        fast -> detect(img, keypoints);
+    }
+    else if (detectorType.compare("BIRSK") == 0) {
+        auto brisk = cv::BRISK::create(); 
+        brisk -> detect(img, keypoints);
+    }
+    else if (detectorType.compare("ORB") == 0) {
+        auto orb = cv::ORB::create(); 
+        orb -> detect(img, keypoints);
+    }
+    else if (detectorType.compare("AKAZE") == 0) {
+        auto akaze = cv::AKAZE::create(); 
+        akaze -> detect(img, keypoints);
+    }
+    else if (detectorType.compare("SIFT") == 0) {
+        auto sift = cv::xfeatures2d::SIFT::create(); 
+        sift -> detect(img, keypoints);
+    }
+    else
+    {
+
+        //...
+        // Throw invalid argument error
+        throw invalid_argument("Invalid detector type: " + detectorType);
+    }
+
+    // visualize keypoints
+    if (bVis) {
+        visImage = img.clone();
+        cv::drawKeypoints(img, kptsFAST, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        windowName = detectorType + " Keypoints Detection Results";
+        cv::namedWindow(windowName, 2);
         imshow(windowName, visImage);
         cv::waitKey(0);
     }
