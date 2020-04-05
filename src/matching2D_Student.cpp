434799ad23f52669c7ccd6121361cd8e4f99e760
jinchaolu <30065376+jinchaolu@users.noreply.github.com>
2020-04-05 14:57:59 -0400

    feat: Complete Task MP.5
    Add FLANN as an alternative to brute-force as well as the K-Nearest-Neighbor approach.

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -16,9 +16,18 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
         int normType = cv::NORM_HAMMING;
         matcher = cv::BFMatcher::create(normType, crossCheck);
     }
+    //// TASK MP.5 -> add FLANN matching in file matching2D.cpp
     else if (matcherType.compare("MAT_FLANN") == 0)
     {
         // ...
+        if (descSource.type() != CV_32F)
+        { // OpenCV bug workaround : convert binary descriptors to floating point due to a bug in current OpenCV implementation
+            descSource.convertTo(descSource, CV_32F);
+            descRef.convertTo(descRef, CV_32F);
+        }
+
+        matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
+        // cout << "FLANN matching";
     }
 
     // perform matching task
@@ -27,10 +36,32 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
 
         matcher->match(descSource, descRef, matches); // Finds the best match for each descriptor in desc1
     }
+    //// TASK MP.5 -> add KNN match selection in file matching2D.cpp
     else if (selectorType.compare("SEL_KNN") == 0)
     { // k nearest neighbors (k=2)
 
         // ...
+
+        int k = 2;
+        vector<vector<cv::DMatch>> knn_matches;
+        double t = (double)cv::getTickCount();
+        matcher->knnMatch(descSource, descRef, knn_matches, k); // finds the 2 best matches
+        t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+        cout << " (KNN) with n=" << knn_matches.size() << " matches in " << 1000 * t / 1.0 << " ms" << endl;
+
+        // STUDENT TASK
+        // filter matches using descriptor distance ratio test
+        double minDescDistRatio = 0.8;
+        for (auto it = knn_matches.begin(); it != knn_matches.end(); ++it)
+        {
+
+            if ((*it)[0].distance < minDescDistRatio * (*it)[1].distance)
+            {
+                matches.push_back((*it)[0]);
+            }
+        }
+        cout << "# keypoints removed = " << knn_matches.size() - matches.size() << endl;
+        // EOF STUDENT TASK
     }
 }
 
